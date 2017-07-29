9d06a8966b9f5433da311b2406fe71495321f050
J Silverstein <jess.pebble@gmail.com>
2017-07-29 12:43:47 -0400

    add auth services

---
+++ services/user/passport.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const User = require('../../models/user');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.username);
+  });
+
+  passport.deserializeUser((username, done) => {
+    User.findByUserName(username)
+      .then(user => {
+        done(null, user);
+      })
+      .catch(err => {
+        console.log(err);
+        done(err, null);
+      });
+  });
+};
